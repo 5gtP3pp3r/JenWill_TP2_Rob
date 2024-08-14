25b9f0c636b1707df97a3df34c526ebccff3c2c8
William Crépault <william.crepault@gmail.com>
2024-08-13 22:32:04 -0400

    ajout code Queue

--- Code/Queue.cpp
+++ Code/Queue.cpp
@@ -15,20 +15,48 @@ Queue::~Queue()
 
 Node* Queue::getLastNode() const
 {
-	return NULL;
+	return this->lastNode;
 }
 
 void Queue::add(Block* block)
 {
-	
+	Node* newNode = new Node;
+	newNode->setNext(NULL);
+	newNode->setBlock(block);
+	if (this->lastNode == NULL)
+	{
+		this->setFirstNode(newNode);
+	}
+	else
+	{
+		this->lastNode->setNext(newNode);
+	}
+	this->lastNode = newNode;
 }
 
 Block* Queue::remove()
 {
-	return NULL;
+	if (this->getFirstNode() == NULL)
+	{
+		return NULL;
+	}
+	Node* currentNode = this->getFirstNode();
+	setFirstNode(currentNode->getNext());
+	if (this->getFirstNode() == NULL)
+	{
+		this->lastNode = NULL;
+	}
+	Block* currentBlock = currentNode->getBlock();
+	delete currentNode;
+	return currentBlock;
 }
 
 void Queue::display() const
 {
-	
+	Node* currentNode = getFirstNode();
+	while (currentNode != NULL)
+	{
+		cout << currentNode->getBlock()->points;
+		currentNode = currentNode->getNext();
+	}
 }
