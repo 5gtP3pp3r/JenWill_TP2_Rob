2280a916f26b8891cf50dee5ada1624aeb493570
William Crépault <william.crepault@gmail.com>
2024-08-16 01:48:00 -0400

    Ajout de plein de cout de debug, ajout de commentaires  et quelque mods algo (initialisation blocks et attribution des valeurs) la boucle du constructeur de Cube fonctionne

--- Code/Cube.cpp
+++ Code/Cube.cpp
@@ -17,7 +17,7 @@ Cube::Cube(string cubePath)
 	if (streamInput)
 	{
 		int y = 0;
-		int z = -1;
+		int z = -1;														// Le fichier commence par un "+" ce qui fok le premier incrément de "z" à la ligne 25
 		while (getline(streamInput, currentLine))
 		{
 			if (currentLine == "+")
@@ -27,24 +27,29 @@ Cube::Cube(string cubePath)
 			}
 			else
 			{
+				cout << endl;
 				for (int x = 0; x < DIMENSION; x++)
 				{
-					tabBlocks[x][y][z] = new Block;
-					currentBlock = tabBlocks[x][y][z];
-					currentBlock->x = x;
+					tabBlocks[x][y][z] = new Block;						// Instanciation de tout les blocs à chaque index et non une seule fois.. oups lol
+					cout << "Tab position: [" << x << ", " << y << ", " << z << "]" << endl;
+					cout << "Tab address: \"" << *(&tabBlocks[x][y][z]) << "\"" << endl;
+					currentBlock = tabBlocks[x][y][z];					// Attribution d'adresse et non des valeurs x, y, z     encore oups
+					currentBlock->x = x;								// Attribution des valeurs x, y, z
 					currentBlock->y = y;
 					currentBlock->z = z;
+					cout << "Block coordinates: [ " << currentBlock->x << ", " << currentBlock->y << ", " << currentBlock->z << " ]" << endl;
+					cout << "Block address: \"" << *(&currentBlock) << "\"" << endl ;
 
 					if (isdigit(currentLine[x])) {
-						currentBlock->points = currentLine[x];
+						currentBlock->points = currentLine[x] - '0';	// Remarqué qu'on doit additionner des int plus tard, transfert du char du .txt en int  
 						currentBlock->value = ' ';
 					}
 					else {
 						currentBlock->points = 0;
 						currentBlock->value = currentLine[x];
 					}
-
-					currentBlock->value = currentLine[x];
+					cout << "Block value: (" << currentBlock->value << ") Block points: (" << currentBlock->points << ")" << endl << endl;
+					// currentBlock->value = currentLine[x];   en trop
 
 					if (currentLine[x] == 'S')
 					{
@@ -67,12 +72,16 @@ Cube::Cube(string cubePath)
 					}
 				}
 				y++;
-			}
+			}		
 		}
 		streamInput.close();
 	}
-}
-
+	cout << endl << "Start Block coordinates: [ " << startBlock->x << ", " << startBlock->y << ", " << startBlock->z << " ]" << endl;
+	cout << "Start Block value: (" << startBlock->value << ") Start Block points: (" << startBlock->points << ")" << endl;
+	cout << "Start Block address: \"" << *(&startBlock) << "\"" << endl;
+}		/********** On a un trouble de retour null à la méthode getStartBlock(). Ajout de plein de cout pour confirmer les index valeurs des attributs et adresses **********/
+		/******* Les affichage montre bien que les adresses et que les transferts d'adresses entre les positions du tableau, des blocks et même de startBlock suivent *******/
+		/******* Le problème doit venir au moment d'appeler la méthode getStartBlock() dans Rob et sont utilisation au autre *******/
 
 Cube::~Cube()
 {
